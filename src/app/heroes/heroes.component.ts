5260fe716750948efbf69d7c60dde6298bc88ccf
Krittika <Krittika@DESKTOP-TLP8QUF>
2019-07-24 19:03:43 +0530

    second

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -13,12 +13,12 @@ export class HeroesComponent implements OnInit {
     //name: "Windstorm"
   //};
 
-  heroes = HEROES;
+  heroes: Hero[];
 
   selectedHero: Hero;
 
 
-  constructor() 
+  constructor(private heroService: HeroService) 
   { 
     
   }
